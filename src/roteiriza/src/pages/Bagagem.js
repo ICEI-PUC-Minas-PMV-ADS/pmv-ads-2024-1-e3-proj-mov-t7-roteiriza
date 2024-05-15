2254132da6403e72c6504e86e4ba3bc48eeaac2b
Bruna Santos <s_bruna@live.com>
2024-05-14 22:53:00 -0300

    add page mala de viagem to bagagem file.

--- src/roteiriza/src/pages/Bagagem.js
+++ src/roteiriza/src/pages/Bagagem.js
@@ -4,6 +4,11 @@ import Typography, { TypographyStyles } from '../components/Typography';
 import Header from '../components/Header';
 import Container from '../components/Container';
 import Body from '../components/Body';
+import { Card } from 'react-native-paper';
+import CheckboxForm from '../components/CheckboxForm';
+
+
+import { Text } from 'react-native';
 
 const Usuario = () => {
   return (
@@ -11,10 +16,16 @@ const Usuario = () => {
       <Header title={'Mala de viagem'} />
 
       <Body>
+      
+        <Card>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        </Card>
         <Typography style={TypographyStyles.bodyText}>
         Adicione abaixo os itens que você deseja levar para sua viagem e monte um checklist!
         </Typography>
+      <CheckboxForm />
       </Body>
+
     </Container>
   );
 };
