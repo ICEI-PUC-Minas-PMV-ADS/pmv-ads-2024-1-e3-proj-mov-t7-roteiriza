2f4593771f36e350cf14d001a6c9026111bfc004
Gabriel Bruno <114627827+GabrielBruno7@users.noreply.github.com>
2024-05-24 12:24:35 -0300

    add icon plus on add travel page

--- src/roteiriza/pages/Viagem01.js
+++ src/roteiriza/pages/Viagem01.js
@@ -1,20 +1,10 @@
-import React, {useState} from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { View, Text, ImageBackground, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, ImageBackground, TouchableOpacity,StyleSheet, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-const Viagem01 = ({ user, handleAuthentication, userId }) => {
+const Viagem01 = () => {
 
   const navigation = useNavigation();
-
-  // Obtendo as dimensões da janela
-  const windowWidth = Dimensions.get('window').width;
-  const windowHeight = Dimensions.get('window').height;
-
-  // Proporções definidas para atender às dimensões especificadas
-  const mapContainerHeight = 660 / 1000 * windowHeight;
-
-  const gap = 30; // Espaço entre os contêineres
 
   const handleRederect = () => {
     navigation.navigate('Adicionar Viagem')
@@ -22,25 +12,27 @@ const Viagem01 = ({ user, handleAuthentication, userId }) => {
 
   return (
     <View style={styles.container}>
-      {/* Map Container */}
       <View style={[styles.mapContainer, { flex: 0.8 }]}>
         <ImageBackground
           source={require('../assets/mapanovaviagem.jpg')}
-          style={[styles.image, { marginTop: 50, marginBottom: 50 }]} // Adicionando margem superior e inferior
+          style={[styles.image, { marginTop: 50, marginBottom: 50 }]}
           resizeMode="cover"
         >
           <View style={styles.overlay} />
         </ImageBackground>
       </View>
       
-      {/* Box Container */}
       <View style={[styles.box, { flex: 0.4, marginTop: -20 }]}>
-        <Text style={styles.title}>Roteirize{"\n"}sua viagem</Text>
-        <Text style={styles.subtitle}>Adicione informações sobre o seu roteiro, hospedagem, alimentação, organização da mala e muito mais.</Text>
-        {/* Add Button */}
+        <Text style={styles.title}>
+          Roteirize{"\n"}sua viagem
+        </Text>
+        <Text style={styles.subtitle}>
+          Adicione informações sobre o seu roteiro, hospedagem, alimentação, organização da mala e muito mais.
+        </Text>
+
         <TouchableOpacity style={styles.addButton} onPress={handleRederect}>    
           <View style={styles.iconCircle}>
-            <Ionicons name="add" size={24} color="#ffffff" />
+            <Image source={require('../assets/icons/icon.png')} />
           </View>
         </TouchableOpacity>
       </View>
@@ -48,20 +40,19 @@ const Viagem01 = ({ user, handleAuthentication, userId }) => {
   );
 };
 
-// Estilos CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginBottom:20,
-    flexDirection: 'column', // Alterando a direção do flexbox para vertical
-    padding: 10, // Adicionando um padding para criar uma margem
+    flexDirection: 'column',
+    padding: 10,
   },
   mapContainer: {
     flex: 1,
   },
   image: {
     flex: 1,
-    width: '100%', // Utilizando toda a largura disponível
+    width: '100%',
     borderRadius: 20,
     overflow: 'hidden',
   },
@@ -73,7 +64,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#063A7A',
     borderRadius: 20,
     padding: 20,
-    marginBottom: 10, // Espaço entre a caixa e a parte inferior da tela
+    marginBottom: 10,
   },
   title: {
     fontSize: 24,
@@ -85,10 +76,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 30,
     color: '#ffffff',
-    marginTop: 10, // Adicionando margem superior
+    marginTop: 10,
   },
   addButton: {
-    alignItems: 'center', // Centralizando conteúdo horizontalmente
+    alignItems: 'center',
     marginTop: 10,
   },
   iconCircle: {
@@ -97,7 +88,7 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 25,
     justifyContent: 'center',
-    alignItems: 'center', // Centralizando conteúdo horizontalmente e verticalmente
+    alignItems: 'center',
   },
 });
 
