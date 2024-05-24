47623294ff38f4173d179fa2121adcdd48a4c8f5
Gabriel Bruno <114627827+GabrielBruno7@users.noreply.github.com>
2024-05-24 11:45:18 -0300

    Bottombar navigation icons fix

--- src/roteiriza/pages/Navigation.js
+++ src/roteiriza/pages/Navigation.js
@@ -1,44 +1,60 @@
 import React, { useState } from 'react';
 import { BottomNavigation } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
-
-import Viagem01 from './Viagem01';
-import Viagem02 from './Viagem02';
-import Viagem from './pagina2/Viagem';
-import MeusPasseios from './MeusPasseios';
-import Usuario from './pagina2/Usuario';
-import Hospedagem from './Hospedagem';
-import Home from './HomeViagem';
-import SubMenu from './SubMenu';
-import Passagem from './Passagem';
-
+import { StyleSheet, Image } from 'react-native';
 import StackNavigation from './StackNavigation';
 
-const Navigation = ({ user, handleAuthentication, userId, objectUser}) => {
-  
+  const Imagem = ({ source }) => (
+    <Image source={source} style={{ width: 24, height: 24}} />
+  );
+
+  const Navigation = ({ user, handleAuthentication, userId, objectUser }) => {
   const [index, setIndex] = useState(0);
+
   const [routes] = useState([
-    { key: 'home', icon: 'home'},
-    { key: 'viagem', icon: 'dots-grid'},
-    { key: 'usuario', icon: 'account'},
+    { key: 'home', icon: () =>  <Imagem source={require('../assets/icons/home_white.png')} /> },
+    { key: 'viagem', icon: () => <Imagem source={require('../assets/icons/plane_white.png')} /> },
+    { key: 'usuario', icon: () => <Imagem source={require('../assets/icons/user_white.png')} /> },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
-    home:   () => <StackNavigation type='home' user={user} handleAuthentication={handleAuthentication} userId = {userId} objectUser={objectUser} />,
-    viagem: () => <StackNavigation type='viagem' user={user} handleAuthentication={handleAuthentication} objectUser={objectUser} userId = {userId} />,
-    usuario:() => <StackNavigation type='usuario' user={user} handleAuthentication={handleAuthentication} objectUser = {objectUser}/>,
+
+    home: () =>
+      <StackNavigation 
+        type='home'
+        user={user}
+        handleAuthentication={handleAuthentication}
+        userId={userId}
+        objectUser={objectUser}
+      />,
+
+    viagem: () =>
+      <StackNavigation 
+        type='viagem'
+        user={user}
+        handleAuthentication={handleAuthentication}
+        objectUser={objectUser}
+        userId={userId}
+      />,
+
+    usuario: () => 
+      <StackNavigation 
+        type='usuario'
+        user={user}
+        handleAuthentication={handleAuthentication}
+        objectUser={objectUser}
+      />,
   });
 
   return (
-    <BottomNavigation
-      navigationState={{ index, routes }}
-      onIndexChange={setIndex}
-      renderScene={renderScene}
-      barStyle={styles.background}
-    />
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+        renderIcon={({ route }) => route.icon()}
+        barStyle={styles.background}
+      />
   );
 };
-
 
 const styles = StyleSheet.create({
   background: {
