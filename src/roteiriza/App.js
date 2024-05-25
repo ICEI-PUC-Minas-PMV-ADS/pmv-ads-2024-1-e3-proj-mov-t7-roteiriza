9a781c0cfcf66d593c7ae4fff0112f4fb76eff1c
João <joaoalmei.vitorsocial@gmail.com>
2024-05-25 18:56:37 -0300

    roteiro em andamento

--- src/roteiriza/App.js
+++ src/roteiriza/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Viagem01 from '../roteiriza/pages/Viagem01';
 import Viagem02 from '../roteiriza/pages/Viagem02';
+import roteiro from  '../roteiriza/pages/roteiro';
 
 const Stack = createStackNavigator()
 
@@ -9,7 +10,10 @@ export default function App(){
   return(
   <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: true}}>
-
+      <Stack.Screen
+        name = 'Roteiro'
+        component={roteiro}
+      />
       <Stack.Screen
         name = 'Viagem01'
         component={Viagem01}
--- src/roteiriza/pages/roteiro.js
+++ src/roteiriza/pages/roteiro.js
@@ -1,65 +1,240 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native';
-import { Calendar } from 'react-native-calendars';
+import React, { useEffect, useState } from "react";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import { collection, query, where, getDocs } from "@firebase/firestore";
+import { firestore } from "../firebase/config";
+import { TextInput } from "react-native-paper";
+import { Calendar } from "react-native-calendars";
+import "moment/locale/pt-br";
 
-const TravelPlanScreen = () => {
+const Roteiro = ({ viagemId }) => {
+  const [listDados, setListDados] = useState([]);
+  const [listaManha, setListaManha] = useState([]);
+  const [listaTarde, setListaTarde] = useState([]);
+  const [listaNoite, setListaNoite] = useState([]);
+  const [selectedDate, setSelectedDate] = useState("");
+  const [valor, setValor] = useState("");
+
+  const loadDados = async (viagemId) => {
+    try {
+      const passeioCollectionRef = collection(firestore, "passeio");
+      const alimentacaoCollectionRef = collection(firestore, "alimentacao");
+      const passeioQuery = query(
+        passeioCollectionRef,
+        where("viagemId", "==", viagemId)
+      );
+      const alimentacaoQuery = query(
+        alimentacaoCollectionRef,
+        where("viagemId", "==", viagemId)
+      );
+      const [passeioSnapshot, alimentacaoSnapshot] = await Promise.all([
+        getDocs(passeioQuery),
+        getDocs(alimentacaoQuery),
+      ]);
+      const passeioList = passeioSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      const alimentacaoList = alimentacaoSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setListDados([...passeioList, ...alimentacaoList]);
+    } catch (error) {
+      console.log("Ocorreu um erro: ", error);
+    }
+  };
+
+  useEffect(() => {
+    if (viagemId) {
+      loadDados(viagemId);
+    }
+  }, [viagemId]);
+
+  useEffect(() => {
+    if (selectedDate) {
+      filtro(selectedDate);
+    }
+  }, [selectedDate, listDados]);
+
+  const filtro = (selectedDate) => {
+    const filteredListDados = listDados.filter(
+      (item) => item.data === selectedDate
+    );
+    const ListManha = filteredListDados.filter(
+      (item) => item.Horario === "Manha"
+    );
+    const ListTarde = filteredListDados.filter(
+      (item) => item.Horario === "Tarde"
+    );
+    const ListNoite = filteredListDados.filter(
+      (item) => item.Horario === "Noite"
+    );
+
+    let valorAux = 0;
+    for (let i = 0; i < filteredListDados.length; i++) {
+      valorAux += filteredListDados[i].valor || 0;
+    }
+
+    setValor(valorAux);
+    setListaManha(ListManha);
+    setListaTarde(ListTarde);
+    setListaNoite(ListNoite);
+  };
+
+  const today = new Date().toISOString().split("T")[0];
+
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Roteiro de Viagens</Text>
-      </View>
+    <View style={styles.container}>
       <Calendar
-        style={styles.calendar}
-        // Aqui você pode adicionar mais configurações para o calendário, se necessário
+        onDayPress={(day) => setSelectedDate(day.dateString)}
+        markedDates={{
+          [selectedDate]: {
+            selected: true,
+            marked: true,
+            selectedColor: "#063A7A",
+          },
+          [today]: { selected: true, marked: true, selectedColor: "#F5BD60" },
+        }}
+        theme={{
+          todayTextColor: "#F5BD60",
+          arrowColor: "#063A7A",
+          textSectionTitleColor: "#063A7A",
+          selectedDayBackgroundColor: "#063A7A",
+          dotColor: "#063A7A",
+        }}
+        monthFormat={"MMMM yyyy"}
+        locale={"pt-br"}
+        firstDay={1}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Manhã"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Tarde"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Noite"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Valor gasto no dia"
-        keyboardType="numeric"
-      />
-    </ScrollView>
+      <View style={styles.contentContainer}>
+        <ScrollView>
+          <Text style={styles.header}>Manhã</Text>
+          {listaManha.length > 0 ? (
+            listaManha.map((list, index) => (
+              <View key={index} style={styles.boxLista}>
+                <TextInput
+                  label="Nome do Local"
+                  value={list.titulo}
+                  editable={false}
+                  style={styles.input}
+                  theme={{ colors: { text: "#063A7A", primary: "#063A7A" } }}
+                />
+              </View>
+            ))
+          ) : (
+            <View style={styles.noDataBox}>
+              <Text style={styles.noDataText}>
+                Nenhum roteiro para essa data e hora...
+              </Text>
+            </View>
+          )}
+
+          <Text style={styles.header}>Tarde</Text>
+          {listaTarde.length > 0 ? (
+            listaTarde.map((list, index) => (
+              <View key={index} style={styles.boxLista}>
+                <TextInput
+                  label="Nome do Local"
+                  value={list.titulo}
+                  editable={false}
+                  style={styles.input}
+                  theme={{ colors: { text: "#063A7A", primary: "#063A7A" } }}
+                />
+              </View>
+            ))
+          ) : (
+            <View style={styles.noDataBox}>
+              <Text style={styles.noDataText}>
+                Nenhum roteiro para essa data e hora...
+              </Text>
+            </View>
+          )}
+
+          <Text style={styles.header}>Noite</Text>
+          {listaNoite.length > 0 ? (
+            listaNoite.map((list, index) => (
+              <View key={index} style={styles.boxLista}>
+                <TextInput
+                  label="Nome do Local"
+                  value={list.titulo}
+                  editable={false}
+                  style={styles.input}
+                  theme={{ colors: { text: "#063A7A", primary: "#063A7A" } }}
+                />
+              </View>
+            ))
+          ) : (
+            <View style={styles.noDataBox}>
+              <Text style={styles.noDataText}>
+                Nenhum roteiro para essa data e hora...
+              </Text>
+            </View>
+          )}
+
+          <Text style={styles.header}>
+            Valor a ser gasto no dia (expectativa)
+          </Text>
+          <View style={styles.valueBox}>
+            <TextInput
+              value={String(valor)}
+              editable={false}
+              label="Valor a ser gasto no dia..."
+              style={styles.input} 
+              theme={{ colors: { text: "#063A7A", primary: "#063A7A" } }}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
+    padding: 10,
+  },
+  contentContainer: {
+    marginTop: 20,
   },
   header: {
-    padding: 20,
-    backgroundColor: '#f8f8f8',
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#e8e8e8',
-  },
-  headerText: {
     fontSize: 20,
-    fontWeight: 'bold',
+    color: "#063A7A",
+    marginVertical: 10,
+    marginLeft: 10,
+    fontWeight: "bold",
   },
-  calendar: {
+  boxLista: {
     marginBottom: 20,
+    marginHorizontal: 10,
+    borderRadius: 15,
+    backgroundColor: "#f5f5f5",
+    padding: 10,
+  },
+  noDataBox: {
+    marginBottom: 20,
+    marginHorizontal: 10,
+    borderRadius: 15,
+    backgroundColor: "#f5f5f5",
+    padding: 10,
   },
   input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    margin: 10,
-    paddingHorizontal: 10,
+    fontSize: 16,
+  },
+  valueBox: {
+    marginBottom: 20,
+    marginHorizontal: 10,
+    padding: 10,
+  },
+  valueInput: {
+    fontSize: 16,
+  },
+  noDataText: {
+    fontSize: 16,
+    color: "gray",
+    textAlign: "center",
   },
 });
 
-export default TravelPlanScreen;
+export default Roteiro;
