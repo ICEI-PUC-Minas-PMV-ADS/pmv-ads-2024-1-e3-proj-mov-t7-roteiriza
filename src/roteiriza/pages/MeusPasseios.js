b80d99080e6c454010078280f5918ecc243e5ad8
Tayane Milagres <devtaay@gmail.com>
2024-06-01 14:46:18 -0300

    [ UPDATE ] Add lista dinamica dos passeios

--- src/roteiriza/pages/MeusPasseios.js
+++ src/roteiriza/pages/MeusPasseios.js
@@ -1,50 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import {Text, View, Image, ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
 import  Header  from '../components/Header'
 import ContainerPasseios from '../components/containerPasseios'
 import Button from '../components/buttonAdicionar';
 import { useNavigation } from '@react-navigation/native';
 
+import { collection, addDoc, query, where, getDocs} from '@firebase/firestore';
+import { app, firestore } from '../firebase/config';
 
 import { useRoute } from '@react-navigation/native';
 import ListaAdicionada from '../components/listaAdicionada';
 import ImageLista from '../components/imageLista';
 
 
-const MeusPasseios = ({userId}) => {
-    
-    const navigation = useNavigation(); 
-
+const MeusPasseios = ({ userId }) => {
+    const navigation = useNavigation();
     const route = useRoute();
     const { viagemId } = route.params;
-    
+
     const [ListPasseios, setListPasseios] = useState([]);
 
-    const loadPasseios = async () => {
+    useEffect(() => {
+        loadPasseios();
+    }, []);
 
+    const loadPasseios = async () => {
         try {
             const q = query(collection(firestore, 'passeios'), where('viagemId', '==', viagemId));
             const querySnapshot = await getDocs(q);
-      
+
             if (!querySnapshot.empty) {
-      
-              const docSnap = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}));
-              
-              setListPasseios(docSnap);
-      
+                const docSnap = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                setListPasseios(docSnap);
             } else {
-              console.log('Sem viagens cadastradas');
-              setListPasseios([]);
+                console.log('Sem passeios cadastrados');
+                setListPasseios([]);
             }
-          } catch (error) {
+        } catch (error) {
             console.log('Ocorreu um erro: ', error);
         }
     };
 
     const handleAdicionar = (passeioId) => {
-        navigation.navigate('Criar Passeio', {viagemId, passeioId})
-    }
-    
+        navigation.navigate('Criar Passeio', { viagemId, passeioId });
+    };
 
     return(
         <View>
@@ -60,10 +59,7 @@ const MeusPasseios = ({userId}) => {
                                 onPress={handleAdicionar(passeio.id)}
                                 Data={passeio.Data}
                                 Horario={passeio.Horario}
-                            />
-                            <TouchableOpacity style={styles.btn1} onPress={handleAdicionar}>
-                                <Text style={[styles.text, { color: '#FFFFFF' }]}>Adicionar</Text>
-                            </TouchableOpacity>
+                            />                          
                         </View>
                         
                     ))
@@ -71,11 +67,11 @@ const MeusPasseios = ({userId}) => {
                     <View style={styles.boxLista}>
                         <Text>Nenhum passeio cadastrado</Text>
 
-                        <TouchableOpacity style={styles.btn1} onPress={handleAdicionar}>
-                            <Text style={[styles.text, { color: '#FFFFFF' }]}>Adicionar</Text>
-                        </TouchableOpacity>
                     </View>
                 )}
+                <TouchableOpacity style={styles.btn1} onPress={handleAdicionar}>
+                    <Text style={[styles.text, { color: '#FFFFFF' }]}>ADICIONAR NOVO LOCAL</Text>
+                </TouchableOpacity>
             </ScrollView>
         </View>
     )
@@ -89,13 +85,14 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     btn1: {
-        width: 160,
+        width: 282,
         height: 50,
         backgroundColor: '#F5BD60',
         borderRadius: 10,
-        marginTop: 10,
+        marginTop: 25,
         justifyContent: 'center',
         alignItems: 'center',
+        alignSelf: 'center'
       },
       text: {
         color: '#063A7A',
